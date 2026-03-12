a887e93c5472706c1c4e59ba24de44e998fe1a43
Jonathan Wood <woodjon@gmail.com>
2026-03-11 21:24:07 -0400

    Small .NET tool sample

---
+++ ToolSample/Program.cs
@@ -0,0 +1,16 @@
+﻿var command = args[0];
+var input = string.Join(" ", args.Skip(1));
+
+switch (command)
+{
+    case "encode":
+        var encoded = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(input));
+        Console.WriteLine(encoded);
+        break;
+    case "decode":
+        var decoded = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(input));
+        Console.WriteLine(decoded);
+        break;
+    default:
+        break;
+}
